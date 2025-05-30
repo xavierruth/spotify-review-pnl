92e5794e4f504aadf716147dac0e67e1e956605e
xavierruth <ruthmarianne.xavier@gmail.com>
2025-05-30 00:16:30 -0300

    finalizando

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, request, jsonify
-from dotenv import load_dotenv
 import os
 import json
 import requests
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Carregar variáveis de ambiente
-load_dotenv()
+# Em ambiente local, carregue o .env
+if os.environ.get("RAILWAY_ENVIRONMENT") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
 
-# Inicializar Firebase Admin com chave do ambiente
+# Pegar chave do Firebase
 firebase_key_json = os.environ.get("FIREBASE_KEY")
 if not firebase_key_json:
     raise ValueError("FIREBASE_KEY environment variable not set")
 
+# Convertendo a chave JSON em dicionário
 firebase_key = json.loads(firebase_key_json)
-cred = credentials.Certificate(firebase_key)
-firebase_admin.initialize_app(cred)
 
-# Iniciar o banco
+# Inicializando o Firebase
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_key)
+    firebase_admin.initialize_app(cred)
+
 db = firestore.client()
 
-# Configuração do Hugging Face
-HF_API_TOKEN = os.environ.get("HF_API_TOKEN")  # Deve estar no .env
+# Hugging Face
+HF_API_TOKEN = os.environ.get("HF_API_TOKEN")
 MODEL_ID = "xavierruth/spotify-pnl"
 HF_API_URL = f"https://api-inference.huggingface.co/models/{MODEL_ID}"
 
@@ -30,7 +34,7 @@ headers = {
     "Authorization": f"Bearer {HF_API_TOKEN}"
 }
 
-# Iniciar Flask
+# Flask app
 app = Flask(__name__)
 
 @app.route("/")
@@ -46,17 +50,16 @@ def predict():
         if not text:
             return jsonify({"error": "Empty review"}), 400
 
-        # Enviar para Hugging Face API
+        # Envio à API Hugging Face
         response = requests.post(HF_API_URL, headers=headers, json={"inputs": text})
         response.raise_for_status()
-
         outputs = response.json()
-        # Espera-se que seja uma lista com dicionários [{"label": "LABEL_2", "score": 0.85}, ...]
+
         predicted = max(outputs, key=lambda x: x['score'])
         label = predicted["label"]
-        rating = int(label.replace("LABEL_", "")) + 1  # LABEL_0 → 1, LABEL_4 → 5
+        rating = int(label.replace("LABEL_", "")) + 1
 
-        # Salvar no Firestore
+        # Salva no Firebase
         db.collection("reviews").add({
             "text": text,
             "rating": rating
@@ -69,4 +72,4 @@ def predict():
         return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
